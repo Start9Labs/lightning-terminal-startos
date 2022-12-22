d02f7a4f7f30451c2abf8aca4e14d65cf6e6ccb1
Dread <34528298+islandbitcoin@users.noreply.github.com>
2022-12-21 19:03:10 -0500

    Feat/x86 - update to v0.8.5 fixed LNC issue (#10)
    * updated for x86 architecture
    
    * fix for backup restore
    
    * fix LNC issue, update to v0.8.5
    
    * updates based on feedback and PR comments
    
    Co-authored-by: islandbitcoin <dread@start9.com>
    Co-authored-by: Chris Guida <chrisguida@gmail.com>

--- scripts/procedures/getConfig.ts
+++ scripts/procedures/getConfig.ts
@@ -22,65 +22,24 @@ export const getConfig: T.ExpectedExports.getConfig = compat.getConfig({
       "len": 22
     }
   },
-  "bitcoind": {
-    "type": "union",
-    "name": "Bitcoin Core",
-    "description": "The Bitcoin Core node to connect to:\n  - internal: The Bitcoin Core or Proxy services installed to your Embassy\n",
-    "tag": {
-      "id": "type",
-      "name": "Type",
-      "variant-names": {
-        "internal": "Bitcoin Core",
-        "internal-proxy": "Bitcoin Proxy"
-      },
-      "description": "The Bitcoin Core node to connect to:\n  - internal: The Bitcoin Core and Proxy services installed to your Embassy\n"
-    },
-    "default": "internal-proxy",
-    "variants": {
-      "internal": {
-        "user": {
-          "type": "pointer",
-          "name": "RPC Username",
-          "description": "The username for Bitcoin Core's RPC interface",
-          "subtype": "package",
-          "package-id": "bitcoind",
-          "target": "config",
-          "multi": false,
-          "selector": "$.rpc.username"
-        },
-        "password": {
-          "type": "pointer",
-          "name": "RPC Password",
-          "description": "The password for Bitcoin Core's RPC interface",
-          "subtype": "package",
-          "package-id": "bitcoind",
-          "target": "config",
-          "multi": false,
-          "selector": "$.rpc.password"
-        }
-      },
-      "internal-proxy": {
-        "user": {
-          "type": "pointer",
-          "name": "RPC Username",
-          "description": "The username for the RPC user allocated to electrs",
-          "subtype": "package",
-          "package-id": "btc-rpc-proxy",
-          "target": "config",
-          "multi": false,
-          "selector": "$.users[?(@.name == \"lightning-terminal\")].name"
-        },
-        "password": {
-          "type": "pointer",
-          "name": "RPC Password",
-          "description": "The password for the RPC user allocated to electrs",
-          "subtype": "package",
-          "package-id": "btc-rpc-proxy",
-          "target": "config",
-          "multi": false,
-          "selector": "$.users[?(@.name == \"lightning-terminal\")].password"
-        },
-      }
-    }
+  "bitcoind-user": {
+    "type": "pointer",
+    "name": "RPC Username",
+    "description": "The username for Bitcoin Core's RPC interface",
+    "subtype": "package",
+    "package-id": "bitcoind",
+    "target": "config",
+    "multi": false,
+    "selector": "$.rpc.username"
   },
+  "bitcoind-password": {
+    "type": "pointer",
+    "name": "RPC Password",
+    "description": "The password for Bitcoin Core's RPC interface",
+    "subtype": "package",
+    "package-id": "bitcoind",
+    "target": "config",
+    "multi": false,
+    "selector": "$.rpc.password"
+  }
 });
