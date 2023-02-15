678991a3492b96cc29759f34f9317c530961dc7e
islandbitcoin <dread@start9.com>
2023-02-14 19:01:54 -0500

    rsync backups

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,2 +1,1 @@
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.1/mod.ts";
-export * from "https://deno.land/x/embassyd_sdk@v0.3.1.0.12/emvar-lite/mod.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.8/mod.ts";
