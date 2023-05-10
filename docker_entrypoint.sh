aafd4bd18ec65f0f50d3cb5b03d0683931224bfe
Dread <34528298+islandbitcoin@users.noreply.github.com>
2023-05-10 14:43:38 -0400

    update to v0.9.1 and bugfix (#14)
    Co-authored-by: islandbitcoin <dread@start9.com>

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -5,25 +5,13 @@ _term() {
   kill -TERM "$lightning_terminal_process" 2>/dev/null
 }
 # Setting variables
+sleep 5
 LND_ADDRESS='lnd.embassy'
 LITD_PASS=$(yq e '.password' /data/start9/config.yaml)
 RPC_USER=$(yq e '.bitcoind-user' /data/start9/config.yaml)
 RPC_PASS=$(yq e '.bitcoind-password' /data/start9/config.yaml)
 RPC_HOST="bitcoind.embassy"
-echo "Running on Bitcoin Core..."
-
 MACAROON_HEADER="Grpc-Metadata-macaroon: $(xxd -ps -u -c 1000 /mnt/lnd/admin.macaroon)"
-until curl --silent --fail --cacert /mnt/lnd/tls.cert --header "$MACAROON_HEADER" https://lnd.embassy:8080/v1/getinfo &>/dev/null
-do
-    echo "LND Server is unreachable. Are you sure the LND service is running?" 
-    sleep 5
-done
-echo "Configuring LiT..."
-
-# Removing any old data in the lit folder
-rm -f /data/.lit/mainnet/lit.macaroon
-# Copying the TLS cert for LND to the faraday working mainnet folder
-cp /data/.lit/tls.cert /data/.faraday/mainnet/tls.cert
 # Creating lit.conf
 echo "
 remote.lnd.rpcserver="$LND_ADDRESS":10009
@@ -34,6 +22,17 @@ faraday.bitcoin.host="$RPC_HOST":8332
 faraday.bitcoin.user="$RPC_USER"
 faraday.bitcoin.password="$RPC_PASS"
 " > /data/.lit/lit.conf
+echo "Running on Bitcoin Core..."
+until curl --silent --fail --cacert /mnt/lnd/tls.cert --header "$MACAROON_HEADER" https://lnd.embassy:8080/v1/getinfo &>/dev/null
+do
+    echo "LND Server is unreachable. Are you sure the LND service is running?" 
+    sleep 5
+done
+echo "Configuring LiT..."
+# Removing any old data in the lit folder
+rm -f /data/.lit/mainnet/lit.macaroon
+# Copying the TLS cert for LND to the faraday working mainnet folder
+cp /data/.lit/tls.cert /data/.faraday/mainnet/tls.cert
 # Properties Page showing password to be used for login
   echo 'version: 2' > /data/start9/stats.yaml
   echo 'data:' >> /data/start9/stats.yaml
@@ -47,7 +46,8 @@ faraday.bitcoin.password="$RPC_PASS"
 
 echo "Starting LiT..."
 /bin/litd --uipassword=$LITD_PASS --macaroonpath=/data/.lit/mainnet/lit.macaroon --lit-dir=/data/.lit --tlscertpath=/data/.lit/tls.cert --tlskeypath=/data/.lit/tls.key --insecure-httplisten=lightning-terminal.embassy:8443 & 
-$lightning_terminal_process=$!
+lightning_terminal_process=$!
+
 
 trap _term SIGTERM
 
