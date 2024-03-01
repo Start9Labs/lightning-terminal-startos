5baa88579ea1665c61d060fbb6b7ea286818d45f
gStart9 <106188942+gStart9@users.noreply.github.com>
2024-03-01 19:04:23 +0000

    Fix for error on backup of dirs to rsync with sticky bits set (#29)

--- scripts/procedures/backups.ts
+++ scripts/procedures/backups.ts
@@ -1,3 +1,7 @@
 import { Backups } from "../deps.ts";
 
-export const { createBackup, restoreBackup } = Backups.volumes("main").build();
+export const { createBackup, restoreBackup } = Backups.with_options({
+  exclude: [".lnd"],
+})
+  .volumes("main")
+  .build();
