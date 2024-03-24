e52e374d19630137abc4471de86e4843800504b8
Shadowy Super Coder <musashidisciple@proton.me>
2024-03-24 09:41:19 -0600

    update upstream to v0.12.4

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration =
-  compat.migrations.fromMapping({}, "0.12.3");
+  compat.migrations.fromMapping({}, "0.12.4");
