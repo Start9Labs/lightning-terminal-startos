eee0fae56f180501c817f7b2ead0dbcdf5da211f
Matt Hill <mattnine@protonmail.com>
2024-04-04 20:43:47 -0600

    exclude .lnd dir

--- startos/backups.ts
+++ startos/backups.ts
@@ -1,3 +1,7 @@
 import { sdk } from './sdk'
 
-export const { createBackup, restoreBackup } = sdk.setupBackups('main')
+export const { createBackup, restoreBackup } = sdk.setupBackups(
+  sdk.Backups.volumes('main').setOptions({
+    exclude: ['.lnd'], // @TODO is this necessary?
+  }),
+)
