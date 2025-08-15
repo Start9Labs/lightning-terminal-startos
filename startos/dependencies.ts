1936a2e8bc3310587bca096a8481f309d386aff4
Shadowy Super Coder <musashidisciple@proton.me>
2025-08-15 12:30:17 -0600

    require latest lnd

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -3,7 +3,7 @@ import { sdk } from './sdk'
 export const setDependencies = sdk.setupDependencies(async ({ effects }) => ({
   lnd: {
     kind: 'running',
-    versionRange: '>=0.17.1 <0.19.0',
+    versionRange: '>=0.19.2-beta:1-beta.1',
     healthChecks: [],
   },
 }))
