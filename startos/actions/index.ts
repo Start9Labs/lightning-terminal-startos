4c08095b6d9e4690fe8bd5a903feb7dbc3fbea98
Shadowy Super Coder <musashidisciple@proton.me>
2025-08-29 13:55:16 -0600

    remove displayPassword action for uniformity

--- startos/actions/index.ts
+++ startos/actions/index.ts
@@ -1,7 +1,5 @@
 import { sdk } from '../sdk'
-import { displayPassword } from './displayPassword'
 import { resetPassword } from './resetPassword'
 
 export const actions = sdk.Actions.of()
   .addAction(resetPassword)
-  .addAction(displayPassword)
