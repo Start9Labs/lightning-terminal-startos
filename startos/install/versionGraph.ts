493650e85864240a2ff1dfeaaae1adc656e923af
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-01 14:17:05 -0600

    resetPass on preInstall

--- startos/install/versionGraph.ts
+++ startos/install/versionGraph.ts
@@ -2,11 +2,16 @@ import { VersionGraph } from '@start9labs/start-sdk'
 import { current, other } from './versions'
 import { litConfig } from '../fileModels/lit.conf'
 import { configDefaults } from '../utils'
+import { sdk } from '../sdk'
+import { resetPassword } from '../actions/resetPassword'
 
 export const versionGraph = VersionGraph.of({
   current,
   other,
   preInstall: async (effects) => {
     await litConfig.write(effects, configDefaults)
+    await sdk.action.createOwnTask(effects, resetPassword, 'critical', {
+      reason: 'Create your LiT admin password',
+    })
   },
 })
