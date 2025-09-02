03546a2fb67bdeac50afd67bdcc87002ddcc092c
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-02 17:55:46 -0600

    fix 0351 migration

--- startos/install/versions/v0.15.2.1-alpha.0.ts
+++ startos/install/versions/v0.15.2.1-alpha.0.ts
@@ -1,34 +1,29 @@
-import {
-  VersionInfo,
-  IMPOSSIBLE,
-  FileHelper,
-  matches,
-} from '@start9labs/start-sdk'
-import { rm } from 'fs/promises'
+import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+import { readFile, rm } from 'fs/promises'
 import { litConfig } from '../../fileModels/lit.conf'
 import { configDefaults } from '../../utils'
+import { load } from 'js-yaml'
 
 export const v0_15_2_1_alpha_0 = VersionInfo.of({
   version: '0.15.2-alpha:1-alpha.0',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {
-      // get old password if exists
-      const uipassword = await FileHelper.yaml(
-        {
-          volumeId: 'main',
-          subpath: '/media/startos/volumes/main/start9/config.yaml',
-        },
-        matches.object({ password: matches.string.optional() }),
-      )
-        .read((c) => c.password)
-        .once()
+      const oldConfigFile = await readFile(
+        '/media/startos/volumes/main/start9/config.yaml',
+        'utf-8',
+      ).catch(console.error)
 
       await litConfig.write(effects, configDefaults)
 
-      if (uipassword) {
+      if (oldConfigFile) {
+        const uipassword = (
+          load(oldConfigFile) as {
+            password: string
+          }
+        ).password
         await litConfig.merge(effects, {
-          uipassword,
+          uipassword: uipassword,
         })
       }
 
