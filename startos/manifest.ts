6974bf40a383f949f4081563c9bcf898a65e65f3
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-02 11:02:19 -0600

    use latest lnd s9pk

--- startos/manifest.ts
+++ startos/manifest.ts
@@ -45,7 +45,7 @@ export const manifest = setupManifest({
     lnd: {
       description: 'Needed to communicate with the Lightning Network',
       optional: false,
-      s9pk: 'https://github.com/Start9Labs/lnd-startos/releases/download/v0.19.2-beta.1-beta.1/lnd.s9pk',
+      s9pk: 'https://github.com/Start9Labs/lnd-startos/releases/download/v0.19.2-beta.1-beta.2/lnd.s9pk',
     },
   },
 })
