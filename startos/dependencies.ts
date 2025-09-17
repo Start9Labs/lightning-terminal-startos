1ac97e2ef77975eb6bd04c807197ce9d4d0776d7
Shadowy Super Coder <musashidisciple@proton.me>
2025-09-17 14:42:11 -0600

    bump lnd dep version

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -3,7 +3,7 @@ import { sdk } from './sdk'
 export const setDependencies = sdk.setupDependencies(async ({ effects }) => ({
   lnd: {
     kind: 'running',
-    versionRange: '>=0.19.2-beta:1-beta.1',
+    versionRange: '>=0.19.3-beta:1-beta.0',
     healthChecks: [],
   },
 }))
