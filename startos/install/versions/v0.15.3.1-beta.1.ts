5c256f9c43c5bbfc0e445aaac8207fa69e3da899
Shadowy Super Coder <musashidisciple@proton.me>
2025-11-19 22:28:02 -0700

    bump pre-release

--- startos/install/versions/v0.15.3.1-beta.1.ts
+++ startos/install/versions/v0.15.3.1-beta.1.ts
@@ -1,7 +1,7 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
-export const v0_15_3_1_beta0 = VersionInfo.of({
-  version: '0.15.3-alpha:1-beta.0',
+export const v0_15_3_1_beta1 = VersionInfo.of({
+  version: '0.15.3-alpha:1-beta.1',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {},
