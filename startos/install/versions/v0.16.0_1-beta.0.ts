bc9c63372683a72d489ac1371af05244134ea8a9
Matt Hill <mattnine@protonmail.com>
2025-12-21 18:31:27 -0700

    better migration

--- startos/install/versions/v0.16.0_1-beta.0.ts
+++ startos/install/versions/v0.16.0_1-beta.0.ts
@@ -1,36 +1,34 @@
-import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
+import { VersionInfo, IMPOSSIBLE, YAML } from '@start9labs/start-sdk'
 import { readFile, rm } from 'fs/promises'
 import { litConfig } from '../../fileModels/lit.conf'
 import { configDefaults } from '../../utils'
-import { load } from 'js-yaml'
 
 export const v0_16_0_1_beta_1 = VersionInfo.of({
   version: '0.16.0-alpha:1-beta.1',
   releaseNotes: 'Revamped for StartOS 0.4.0',
   migrations: {
     up: async ({ effects }) => {
-      const oldConfigFile = await readFile(
-        '/media/startos/volumes/main/start9/config.yaml',
-        'utf-8',
-      )
-
-      if (oldConfigFile) {
-        const uipassword = (
-          load(oldConfigFile) as {
+      // get old config.yaml
+      const configYaml:
+        | {
             password: string
           }
-        ).password
+        | undefined = await readFile(
+        '/media/startos/volumes/main/start9/config.yaml',
+        'utf-8',
+      ).then(YAML.parse, () => undefined)
 
+      if (configYaml) {
         await litConfig.write(effects, {
           ...configDefaults,
-          uipassword,
+          uipassword: configYaml.password,
+        })
+
+        // remove old start9 dir
+        await rm('/media/startos/volumes/main/start9', {
+          recursive: true,
         })
       }
-
-      // remove old start9 dir
-      await rm('/media/startos/volumes/main/start9', {
-        recursive: true,
-      }).catch(console.log)
     },
     down: IMPOSSIBLE,
   },
