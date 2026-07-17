d6113c219bb55fcb04635d0a64b22ecc028349ac
Matt Hill <9935159+MattDHill@users.noreply.github.com>
2026-07-17 14:00:33 -0600

    freeze migrations into v0.17.0-alpha_2.ts, bump to 0.17.0-alpha:3

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,37 +1,13 @@
-import { VersionInfo, IMPOSSIBLE, YAML } from '@start9labs/start-sdk'
-import { readFile, rm } from 'fs/promises'
-import { litConfig } from '../fileModels/lit.conf'
+import { VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '0.17.0-alpha:2',
+  version: '0.17.0-alpha:3',
   releaseNotes: {
-    en_US: 'Internal updates (start-sdk 2.0.x)',
-    es_ES: 'Actualizaciones internas (start-sdk 2.0.x)',
-    de_DE: 'Interne Aktualisierungen (start-sdk 2.0.x)',
-    pl_PL: 'Aktualizacje wewnętrzne (start-sdk 2.0.x)',
-    fr_FR: 'Mises à jour internes (start-sdk 2.0.x)',
+    en_US: 'Internal updates',
+    es_ES: 'Actualizaciones internas',
+    de_DE: 'Interne Aktualisierungen',
+    pl_PL: 'Aktualizacje wewnętrzne',
+    fr_FR: 'Mises à jour internes',
   },
-  migrations: {
-    up: async ({ effects }) => {
-      const configYaml:
-        | {
-            password: string
-          }
-        | undefined = await readFile(
-        '/media/startos/volumes/main/start9/config.yaml',
-        'utf-8',
-      ).then(YAML.parse, () => undefined)
-
-      if (configYaml) {
-        await litConfig.merge(effects, {
-          uipassword: configYaml.password,
-        })
-
-        await rm('/media/startos/volumes/main/start9', {
-          recursive: true,
-        })
-      }
-    },
-    down: IMPOSSIBLE,
-  },
+  migrations: {},
 })
